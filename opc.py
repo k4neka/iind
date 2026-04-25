edb66b02038f19f8a89ac7ccaab26fea049278a7
Alex <alexandregorra@gmail.com>
2026-04-25 03:55:57 +0100

    Initial commit: OPC UA client script

---
+++ opc.py
@@ -0,0 +1,26 @@
+﻿from opcua import Client, ua
+
+def main():
+    
+    url = "opc.tcp://127.0.0.1:4840"
+    
+    # 2. Initialize the freeopcua Client
+    client = Client(url)
+    
+    try:
+        print(f"Connecting to {url} ...")
+        client.connect()
+        print("Successfully connected to the CODESYS project!")
+
+ 
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        
+    finally:
+        # Cleanly disconnect when finished
+        print("Disconnecting...")
+        client.disconnect()
+
+if __name__ == "__main__":
+    main()
